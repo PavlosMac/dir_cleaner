fbc32cdb8a434440c7df0632390b715a14933bca
pavlos-mac <pavlos.macdonald-kosmidis@uk.clara.net>
2022-03-16 21:04:01 +0000

    * add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 // > get cli args, one for *destination *file types(ext.) *operation
 #![allow(unused)]
 
+use clap::Parser;
 use regex::Regex;
+use std::error::Error;
 use std::ffi::OsStr;
 use std::fs;
 use std::path::{Path, PathBuf};
 
-use clap::Parser;
+type BoxResult<T> = Result<T, Box<dyn Error>>;
 
+// mod error;
 #[derive(Parser)]
 struct Cli {
     #[clap(parse(from_os_str))]
     dir_path: PathBuf,
     extension_str: String,
 }
-fn main() {
+fn main() -> BoxResult<()> {
     let args = Cli::parse();
     let path = &args.dir_path;
     let ext = &args.extension_str.to_owned();
-
-    for entry in std::fs::read_dir(&path).unwrap() {
+    for entry in std::fs::read_dir(&path)? {
         let entry = entry
             .unwrap()
             .path()
@@ -28,11 +30,19 @@ fn main() {
             .to_string_lossy()
             .into_owned();
         let f = format!("\\.(?i:)(?:{})$", &args.extension_str);
-        let r = Regex::new(&f).unwrap();
-        if r.find(entry.as_str()).is_some() {
+        let reg_x = handle_regex(&f).unwrap();
+        println!("{}", entry);
+        if reg_x.find(entry.as_str()).is_some() {
             let p = path.to_string_lossy();
             let f = format!("{}/{}", &p, &entry);
-            std::fs::remove_file(f);
-        }
+            println!("{}", f);
+            // std::fs::remove_file(f);
+        };
     }
+    Ok(())
+}
+
+fn handle_regex(p: &str) -> BoxResult<Regex> {
+    let reg = Regex::new(&p)?;
+    Ok(reg)
 }
